1088da5b4f0db03813329a9e962d33de53414d02
aamaya <alessandro.amaya5@gmail.com>
2022-08-01 08:10:07 -0600

    Ajustes en sincronización de foto y nombre con Google, funcionalidad de ver mensajes en whatsapp y mapa

--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -76,13 +76,45 @@ class UpdateLugarFragment : Fragment() {
             binding.btPlay.isEnabled=false
         }
 
-        //binding.btWhatsapp.setOnClickListener { enviarWhatsapp() }
-        //binding.btLocation.setOnClickListener { verMapa() }
+        binding.btWhatsapp.setOnClickListener { enviarWhatsapp() }
+
+        binding.btLocation.setOnClickListener { verMapa() }
 
         //Se indica que hay opciones de menu
         setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    private fun enviarWhatsapp() {
+        //Se recupera el correo del lugar...
+        val telefono = binding.etTelefono.text.toString()
+        if (telefono.isNotEmpty()) {
+            //Se activa el correo
+            val sendIntent = Intent(Intent.ACTION_VIEW)
+            val uri = "whatsapp://send?phone-506$telefono&text="+getString(R.string.msg_saludos)
+            sendIntent.setPackage("com.whatsapp")
+            sendIntent.data=Uri.parse(uri)
+            startActivity(sendIntent)
+            } else {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.msg_datos),Toast.LENGTH_SHORT
+            ).show()
+            }
+        }
+
+    private fun verMapa() {
+        val latitud=binding.tvLatitud.text.toString().toDouble()
+        val longitud=binding.tvLongitud.text.toString().toDouble()
+
+        if (latitud.isFinite() && longitud.isFinite()){
+            val location = Uri.parse("geo:$latitud,$longitud?z18")
+            val mapIntent = Intent(Intent.ACTION_VIEW,location)
+            startActivity(mapIntent)
+        } else {
+
+        }
     }
 
     private fun verWeb() {
